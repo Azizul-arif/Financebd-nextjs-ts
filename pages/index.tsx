805f2fd0413b334c71f23608e87e48be23b4892e
Azizul Arif <azizularif94@gmail.com>
2022-08-09 00:23:57 +0600

    name changes in components

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { ThemeProvider } from '@emotion/react'
 
-import Navbar from '../components/Navbar'
-import Board from '../components/Board'
+import Navbar from '../components/navbar'
+import Board from '../components/board'
 
 const Home: NextPage = () => {
   return (
