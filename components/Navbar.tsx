15600466f8ae83d6acfbe42a6efa38354ea67244
Azizul Arif <azizularif94@gmail.com>
2022-08-09 00:40:14 +0600

    import fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,60 +1,60 @@
-import { NextPage } from 'next'
-import React from 'react'
-import Image from 'next/image'
+import { NextPage } from 'next';
+import React from 'react';
+import Image from 'next/image';
 
-import { AppBar, Toolbar, Typography, Grid } from '@mui/material'
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
-import AccountCircleIcon from '@mui/icons-material/AccountCircle'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
-import SearchBox from './searchbox'
-import logo from './../assets/Logo.jpg'
+import { AppBar, Toolbar, Typography, Grid } from '@mui/material';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchBox from './Searchbox';
+import logo from './../assets/Logo.jpg';
 
 const styles = {
-  appBar: { bgcolor: 'white', color: 'black', padding: 1 },
-  logo: {
-    marginLeft: '50px',
-    Width: '217px',
-    Height: '36px',
-  },
-}
+	appBar: { bgcolor: 'white', color: 'black', padding: 1 },
+	logo: {
+		marginLeft: '50px',
+		Width: '217px',
+		Height: '36px',
+	},
+};
 
 export default function Navbar() {
-  return (
-    <AppBar elevation={0} position="fixed" sx={styles.appBar}>
-      <Toolbar>
-        <Grid container spacing={3} alignItems="center">
-          <Grid item md={2}>
-            <Image src={logo} style={styles.logo} alt="Logo" />
-          </Grid>
-          <Grid item xs={6}>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Good Morning, Shiva!
-            </Typography>
-          </Grid>
-          <Grid container item xs={4} spacing={3}>
-            <Grid item>
-              <SearchBox />
-            </Grid>
-            <Grid item>
-              <NotificationsNoneIcon
-                sx={{ color: '#7b61FF' }}
-                style={{ marginTop: '12px' }}
-              />
-            </Grid>
-            <Grid
-              item
-              display="flex"
-              justifyContent="center"
-              alignItems="end"
-              style={{ marginBottom: '12px' }}
-            >
-              <AccountCircleIcon />
-              <Typography>Shiva Michelle </Typography>
-              <KeyboardArrowDownIcon />
-            </Grid>
-          </Grid>
-        </Grid>
-      </Toolbar>
-    </AppBar>
-  )
+	return (
+		<AppBar elevation={0} position="fixed" sx={styles.appBar}>
+			<Toolbar>
+				<Grid container spacing={3} alignItems="center">
+					<Grid item md={2}>
+						<Image src={logo} style={styles.logo} alt="Logo" />
+					</Grid>
+					<Grid item xs={6}>
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+							Good Morning, Shiva!
+						</Typography>
+					</Grid>
+					<Grid container item xs={4} spacing={3}>
+						<Grid item>
+							<SearchBox />
+						</Grid>
+						<Grid item>
+							<NotificationsNoneIcon
+								sx={{ color: '#7b61FF' }}
+								style={{ marginTop: '12px' }}
+							/>
+						</Grid>
+						<Grid
+							item
+							display="flex"
+							justifyContent="center"
+							alignItems="end"
+							style={{ marginBottom: '12px' }}
+						>
+							<AccountCircleIcon />
+							<Typography>Shiva Michelle </Typography>
+							<KeyboardArrowDownIcon />
+						</Grid>
+					</Grid>
+				</Grid>
+			</Toolbar>
+		</AppBar>
+	);
 }
--- components/News.tsx
+++ components/News.tsx
@@ -1,68 +1,68 @@
-import React, { useState } from 'react'
-import { Grid, Box, Typography, Paper, ThemeProvider } from '@mui/material'
-import NewsItem from './NewsItem'
+import React, { useState } from 'react';
+import { Grid, Box, Typography, Paper, ThemeProvider } from '@mui/material';
+import NewsItem from './NewsItem';
 
 const styles = {
-  newsHeading: {
-    fontWeight: '600',
-    fontSize: '14px',
-    lineHeight: '25.14px',
-  },
-  newsBar: {
-    Width: '318px',
-    Height: '86px',
-    gap: '16px',
-    padding: '12px',
-    marginTop: '20px',
+	newsHeading: {
+		fontWeight: '600',
+		fontSize: '14px',
+		lineHeight: '25.14px',
+	},
+	newsBar: {
+		Width: '318px',
+		Height: '86px',
+		gap: '16px',
+		padding: '12px',
+		marginTop: '20px',
 
-    fontStyle: 'normal',
-    fontWeight: '600',
-    fontSize: '14px',
-    lineHeight: '18px',
-    border: '1px solid #F4F4F4',
-    borderRadius: '8px',
-    boxSizing: 'border-box',
-    isolation: 'isolate',
-  },
-  date: {
-    fontSize: '12px',
-    lineHeight: '18px',
-    fontWeight: '400',
-    color: '#767676',
-    marginTop: '6px',
-  },
-  heading: {
-    fontWeight: '600',
-    fontSize: '14px',
-    lineHeight: '17.6px',
-    color: '#1A1A1A',
-    borderLeftColor: 'red',
-  },
-}
+		fontStyle: 'normal',
+		fontWeight: '600',
+		fontSize: '14px',
+		lineHeight: '18px',
+		border: '1px solid #F4F4F4',
+		borderRadius: '8px',
+		boxSizing: 'border-box',
+		isolation: 'isolate',
+	},
+	date: {
+		fontSize: '12px',
+		lineHeight: '18px',
+		fontWeight: '400',
+		color: '#767676',
+		marginTop: '6px',
+	},
+	heading: {
+		fontWeight: '600',
+		fontSize: '14px',
+		lineHeight: '17.6px',
+		color: '#1A1A1A',
+		borderLeftColor: 'red',
+	},
+};
 
-const number = [1, 2, 3, 4, 5, 6, 7, 8]
+const number = [1, 2, 3, 4, 5, 6, 7, 8];
 // const data = {
 // 	news: string,
 // 	date: string,
 // };
 
 const News = () => {
-  return (
-    <div>
-      <Typography variant="h5" sx={styles.newsHeading}>
-        News
-      </Typography>
+	return (
+		<div>
+			<Typography variant="h5" sx={styles.newsHeading}>
+				News
+			</Typography>
 
-      {number.map((item, i) => {
-        return (
-          <NewsItem
-            title="Fades preferred infaltion measures rose 4.7% in May,around multi-decade heights"
-            date="Jan 22,2022"
-          />
-        )
-      })}
-    </div>
-  )
-}
+			{number.map((item, i) => {
+				return (
+					<NewsItem
+						title="Fades preferred infaltion measures rose 4.7% in May,around multi-decade heights"
+						date="Jan 22,2022"
+					/>
+				);
+			})}
+		</div>
+	);
+};
 
-export default News
+export default News;
